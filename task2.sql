91c9d8f1d0e2c64ab841989d302f1543a136b13e
Kibalov <kvv.20@uni-dubna.ru>
2022-04-02 18:46:25 +0300

    lesson

---
+++ task2.sql
@@ -0,0 +1,61 @@
+-- N.1
+SELECT stud.name, stud.surname
+FROM student stud
+WHERE score > 4 and score < 4.5
+
+-- N.2
+SELECT stud.name::text, CAST(stud.surname AS varchar), stud.score::varchar
+FROM student stud
+WHERE score > 4 and score < 4.5
+
+-- N.3
+SELECT stud.uni_group, stud.name, stud.surname
+FROM student stud
+ORDER BY stud.uni_group, stud.name 
+
+-- N.4
+SELECT stud.name, stud.surname, stud.score
+FROM student stud
+WHERE stud.score > 4
+ORDER BY stud.score desc
+
+-- N.5
+SELECT *
+FROM hobby hobb
+WHERE hobb.name='swiming' or hobb.name='surfing'
+
+-- N.6
+SELECT stho.n_z, stho.id_hobby
+FROM stud_hobby stho
+WHERE stho.date_start between '2011/02/25' and '2017/02/27' and stho.date_finish IS NULL
+
+-- N.7
+SELECT stud.name, stud.surname, stud.score
+FROM student stud
+WHERE stud.score > 4.5
+ORDER BY stud.score desc
+
+-- N.8
+SELECT *
+FROM student stud
+WHERE stud.score > 4.5
+ORDER BY stud.score desc
+LIMIT 3
+
+-- N.9
+SELECT *,
+ CASE 
+ WHEN hobb.risk < 2 THEN 'Очень низкий'
+ WHEN hobb.risk >= 2 and hobb.risk < 4 THEN 'Низкий' 
+ WHEN hobb.risk >= 4 and hobb.risk < 6 THEN 'Средний' 
+ WHEN hobb.risk >= 6 and hobb.risk < 8 THEN 'Высокий' 
+ WHEN hobb.risk >= 8 THEN 'Очень высокий' 
+ ELSE 'Мы точно никогда не выведем это сообщение'
+END
+FROM hobby hobb
+
+-- N.10
+SELECT *
+FROM hobby hobb
+ORDER BY hobb.risk desc
+LIMIT 3
