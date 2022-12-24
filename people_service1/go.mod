1fc9753d94f8e170ccd06305f837a69d566f79ab
Kibalov <kvv.20@uni-dubna.ru>
2022-12-24 08:04:57 +0300

    people_service

--- people_service1/go.mod
+++ people_service1/go.mod
@@ -3,12 +3,16 @@ module github.com/Kibalov/Kibalov_Venedikt3254/people_service1
 go 1.19
 
 require (
+	github.com/golang-migrate/migrate/v4 v4.15.2
 	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v4 v4.17.2
+	github.com/lib/pq v1.10.7
 )
 
 require (
 	github.com/gofrs/uuid v4.3.1+incompatible // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
@@ -17,9 +21,9 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.12.0 // indirect
-	github.com/lib/pq v1.10.7 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 )
--- people_service1/service/store/store.go
+++ people_service1/service/store/store.go
@@ -4,6 +4,14 @@ import (
 	"context"
 
 	"github.com/jackc/pgx/v4"
+
+	"database/sql"
+
+	_ "github.com/lib/pq"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 type Store struct {
@@ -24,15 +32,51 @@ func NewStore(connString string) *Store {
 
 	// make migration
 
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		panic(err)
+	}
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		panic(err)
+	}
+	m, err := migrate.NewWithDatabaseInstance("file:../migrations", "postgres", driver)
+	if err != nil {
+		panic(err)
+	}
+	m.Up()
+
 	return &Store{
 		conn: conn,
 	}
 }
 
 func (s *Store) ListPeople() ([]People, error) {
-	return nil, nil
+	rows, err := s.conn.Query(context.Background(), "SELECT * FROM people")
+	if err != nil {
+		return nil, err
+	}
+
+	people := make([]People, 0)
+	for rows.Next() {
+		var p People
+		err = rows.Scan(&p.ID, &p.Name)
+		if err != nil {
+			return nil, err
+		}
+		people = append(people, p)
+	}
+
+	return people, nil
+
 }
 
-func (s *Store) GetPeopleByID(id string) (People, error) {
-	return People{}, nil
+func (s *Store) GetPeopleByID(id int) (People, error) {
+	var p People
+	err := s.conn.QueryRow(context.Background(), "SELECT * FROM people WHERE id = $1", id).Scan(&p.ID, &p.Name)
+	if err != nil {
+		return People{}, err
+	}
+
+	return p, nil
 }
